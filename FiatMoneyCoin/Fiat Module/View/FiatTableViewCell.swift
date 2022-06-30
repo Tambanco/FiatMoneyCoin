eb5e65ac8a3663514ce72b0c11a8aac058d847bd
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-06-30 12:14:21 +0300

    Refactor to MVP

---
+++ FiatMoneyCoin/Fiat Module/View/FiatTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FiatTableViewCell.swift
+//  FiatMoneyCoin
+//
+//  Created by tambanco 🥳 on 30.06.2022.
+//
+
+import UIKit
+
+class FiatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
