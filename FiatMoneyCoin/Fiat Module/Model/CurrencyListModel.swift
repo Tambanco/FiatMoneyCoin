65f962cdac060416944bd6d11aa40d753078b0b9
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-06-30 19:02:23 +0300

    Add CurrencyListModel

---
+++ FiatMoneyCoin/Fiat Module/Model/CurrencyListModel.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyListModel.swift
+//  FiatMoneyCoin
+//
+//  Created by tambanco 🥳 on 30.06.2022.
+//
+
+import Foundation
+
+struct CurrencyListModel: Codable {
+    let success: Bool
+    let symbols: [String: String]
+}
