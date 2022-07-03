95e8f3e0517d41e30341544f32f279161fd714db
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-07-03 11:59:05 +0300

    Add PickerView class

---
+++ FiatMoneyCoin/Service Layer/PickerView.swift
@@ -0,0 +1,20 @@
+//
+//  PickerView.swift
+//  FiatMoneyCoin
+//
+//  Created by tambanco 🥳 on 03.07.2022.
+//
+
+import UIKit
+
+class PickerView: UIPickerView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
