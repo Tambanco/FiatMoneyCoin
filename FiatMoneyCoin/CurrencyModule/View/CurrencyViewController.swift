dfc7c79ba458175b293464045060b22e7ee200d4
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-07-06 13:55:01 +0300

    Update CurrencyViewController.swift
    add keyboardWillShow
    keyboardWillHide methods

--- FiatMoneyCoin/CurrencyModule/View/CurrencyViewController.swift
+++ FiatMoneyCoin/CurrencyModule/View/CurrencyViewController.swift
@@ -10,23 +10,35 @@
 import UIKit
 
 class CurrencyViewController: UIViewController {
-
-	var presenter: CurrencyPresenterProtocol!
+    
+    var presenter: CurrencyPresenterProtocol!
     
     var totalValue: String = ""
     var currencyList: [String] = []
-
+    
     @IBOutlet weak var addCurrencyTF: UITextField!
     @IBOutlet weak var currencyPV: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        presenter.getCurrencyList()
+//        presenter.getCurrencyList()
         setupCurrencyView()
         setupPickerView()
     }
     
     func  setupCurrencyView() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            self.view.frame.origin.y -= keyboardSize.height
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        self.view.frame.origin.y = 0
     }
     
     func setupPickerView() {
