bc44a16300e41de69a492f8e68fbe5b5852d2228
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-07-10 13:04:41 +0300

    Update SceneDelegate.swift
    add NavigationController

--- FiatMoneyCoin/App/SceneDelegate.swift
+++ FiatMoneyCoin/App/SceneDelegate.swift
@@ -11,13 +11,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
+        let assemblyBuilder = AssemblyModuleBuilder()
         let navigationController = UINavigationController()
-        let fiatViewController = FiatViewController()
+        let router = Router(navigationController: navigationController, assemblyBuilder: assemblyBuilder)
+        let amountForCell = ["foo": "bar"]
+        let fiatViewController = assemblyBuilder.createFiatModule(router: router, amountForCell: amountForCell)
         navigationController.viewControllers = [fiatViewController]
         window?.rootViewController = navigationController
         window?.rootViewController?.view.backgroundColor = .white
