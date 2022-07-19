3544697f6b1ef443a9ec7b5e89023cdb662b2907
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-07-19 16:19:42 +0300

    Update FiatPresenterTest.swift

--- FiatMoneyCoinTests/TestFiatModule/FiatPresenterTest.swift
+++ FiatMoneyCoinTests/TestFiatModule/FiatPresenterTest.swift
@@ -8,13 +8,25 @@
 import XCTest
 @testable import FiatMoneyCoin
 
+class MockView: FiatViewProtocol {
+    func updateFiatView() {
+        <#code#>
+    }
+}
+
 class FiatPresenterTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var view: MockView!
+    var presenter: FiatPresenter!
+    var networkService: NetworkServiceProtocol!
+    var router: RouterProtocol!
+    var currencyList = [String]()
+    
+    override func tearDown() {
+        view = nil
+        presenter = nil
+        networkService = nil
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    
+    
+    
 }
