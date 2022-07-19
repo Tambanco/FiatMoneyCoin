978043650aa6a734b2aa42b36b6f288770d7c454
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-07-19 16:12:48 +0300

    Add FiatPresenterTest

---
+++ FiatMoneyCoinTests/TestFiatModule/FiatPresenterTest.swift
@@ -0,0 +1,20 @@
+//
+//  FiatPresenterTest.swift
+//  FiatMoneyCoinTests
+//
+//  Created by tambanco 🥳 on 19.07.2022.
+//
+
+import XCTest
+@testable import FiatMoneyCoin
+
+class FiatPresenterTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
